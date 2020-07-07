74a07230ec7b5d45fa783d8adddd3cf5b62e5870
Jessica <jessictarra@gmail.com>
2020-07-06 21:20:46 -0300

    country

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Country from "./country";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Country
+        flag="https://upload.wikimedia.org/wikipedia/commons/f/f8/Flag_of_Colombia.png"
+        name="Colombia"
+        population={50000000}
+        region="America"
+        capital="Bogota"
+      />
     </div>
   );
 }
