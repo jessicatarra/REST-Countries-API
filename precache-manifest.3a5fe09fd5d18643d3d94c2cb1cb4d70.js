ce65516958d71bd169cfd5e59bc2a280886392b6
Jessica Tarra <jessictarra@gmail.com>
2020-07-09 20:12:46 -0300

    Updates

---
+++ precache-manifest.3a5fe09fd5d18643d3d94c2cb1cb4d70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99298e604b1d6a1c882b31343b907b5",
+    "url": "/REST-Countries-API/index.html"
+  },
+  {
+    "revision": "ea854e916feda9b3600a",
+    "url": "/REST-Countries-API/static/css/main.ecbea91f.chunk.css"
+  },
+  {
+    "revision": "a64ade83791c63922127",
+    "url": "/REST-Countries-API/static/js/2.74b43dc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/REST-Countries-API/static/js/2.74b43dc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea854e916feda9b3600a",
+    "url": "/REST-Countries-API/static/js/main.ae1ae93c.chunk.js"
+  },
+  {
+    "revision": "9fbf0e8542df37f2388d",
+    "url": "/REST-Countries-API/static/js/runtime-main.bf7bfe22.js"
+  }
+]);
